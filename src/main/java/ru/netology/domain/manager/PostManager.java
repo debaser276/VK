15f20ffcb4bc8fca3aac2b3acee4a70e12b9b43e
debaser <debaser86@yandex.ru>
2020-12-27 12:38:46 +0300

    added PostManager.java

---
+++ src/main/java/ru/netology/domain/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.domain.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(long ownerId, String domain, String query, boolean ownersOnly, int count) {
+        return null;
+    }
+
+    public void deletePost(long ownerId, long postId) {
+    }
+}
